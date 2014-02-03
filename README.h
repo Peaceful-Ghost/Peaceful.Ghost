3bfc92a2b3a060e9ffd64fe9cc25662cc968b44b
ghostXcity <ighostxhub@gmail.com>
2014-02-03 00:55:33 -0800

    Updates to the readme
    Lotsa changes

---
+++ README.h
@@ -0,0 +1,31 @@
+*ghostXcitizen-repo:"Peaceful.Ghost"
+// updates to ghost organization
+// peace-in-logic //
+###README.h
+// some organization config.files and a readme
+$git -init
+$git -init -organization --organization.name:"ghostXcitizen"
+$git -init -organization -main.repository:"Peaceful.Ghost"
+$set-organization-owner.names:"ghostXcity"(@ghostXcity.git)&&"ghostXtown"(@ghostXtown.git)
+$git -add README.h --commit README.h -m "First_Commit"
+$-mkdir ~/https://www.github.com/ghostXcitizen/Peaceful.Ghost
+$git -push -master-uname:"ghostXcity" --repository.name:"Peaceful.Ghost"
+$git -push -organization.owner:ghostXcity -organization.name:ghostXcitizen
+$-set-repo:Peaceful.Ghost --master -ghostXcity --bool:true;
+$-echo -set --echo:true
+$git -help --web.commit
+$sudo mkdir -p -/Peaceful.Ghost/share/doc
+$cd -/usr/share/git-doc
+$git-config --global -help.format-web
+$git-info -repo.changes -l-a
+$git-status --show -diff.log
+$git-info -Peaceful.Ghost-page --show author_names --show-contributors
+$git-list:repository -options
+$git-config -list options:all
+$git-config:core.auto-clrf=true
+$git-config--global-set:no-blank-spaces; echo:apply-whitespace:nowarn
+$git-init--alias-all
+$git-ignore--global -remote.add -remote-push -remote-pull etc.remote
+$git -config --global-user.email:ighostxhub@gmail.com:(@ighostxhub.git)
+--set-all-changes:global --echo -set-changes -organization git.log
+$bool-true-all --mark-ctime: (format:"/DD/MM/YYYY")\n;
--- README.md
+++
@@ -1,8 +0,0 @@
-Peaceful.Ghost
-================
-//"peace-in-logic"
-#README.h
-git -add README.h
-git -commit -m "First_Commit"
-git -remote add origin ~/https://www.github.com/ghostXtown/Peaceful.Ghost/
-git -push -u master
